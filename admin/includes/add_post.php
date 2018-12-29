0a20cba3de8facb8da279614d8508ad607bbb963
arlityrk <arlityrk@gmail.com>
2018-12-29 21:44:10 +0200

    112. video commiti bugfixid

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,21 +2,21 @@
 
 if(isset($_POST["create_post"])){
 
-    $post_title = $_GET["title"];
-    $post_category_id = $_GET["post_category_id"];
-    $author = $_GET["author"];
-    $post_status = $_GET["post_status"];
+    $post_title = $_POST["title"];
+    $post_category_id = $_POST["post_category_id"];
+    $author = $_POST["author"];
+    $post_status = $_POST["post_status"];
     $post_image = $_FILES["image"]["name"];
     $post_image_temp = $_FILES["image"]["tmp_name"];
-    $post_tags = $_GET["post_tags"];
-    $post_content = $_GET["post_content"];
+    $post_tags = $_POST["post_tags"];
+    $post_content = $_POST["post_content"];
     $post_date = date("d-m-y");
+    $post_comment_count = 4;
 
     move_uploaded_file($post_image_temp, "../images/$post_image");
-    echo "TEST";
 
     $query = "INSERT INTO posts (post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status)";
-    $query .="VALUES({$post_category_id},'{$post_title}','{$author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_comment_count}','{$post_status}'";
+    $query .=" VALUES({$post_category_id},'{$post_title}','{$author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_comment_count}','{$post_status}')";
 
     $add_post_query = mysqli_query($connection, $query);
 
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,45 +1,64 @@
- <table class="table table-bordered table-hover">
-                        <thead>
-                            <?php
-                            $query = "SELECT * FROM posts order by cat_id desc";
-                            $posts_query = mysqli_query($connection, $query);
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Image</th>
+            <th>Tags</th>
+            <th>Comments</th>
+            <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
 
-                            while($row = mysqli_fetch_assoc($categories_query)){
-                                $post_id = $row['post_id'];
-                                $post_category_id = $row['post_category_id'];
-                                $post_title = $row['post_title'];
-                                $post_author = $row['post_author'];
-                                $post_date = $row['post_date'];
-                                $post_image = $row['post_image'];
-                                $post_content = $row['post_content'];
-                                $post_tags = $row['post_tags'];
-                                $post_comment_count = $row['post_comment_count'];
-                                $post_status = $row['post_status'];
+        <?php
+        $query = "SELECT * FROM posts order by post_id asc";
+        $posts_query = mysqli_query($connection, $query);
+
+        confirm_query($posts_query);
+
+        while($row = mysqli_fetch_assoc($posts_query)){
+            $post_id = $row['post_id'];
+            $post_category_id = $row['post_category_id'];
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            $post_tags = $row['post_tags'];
+            $post_comment_count = $row['post_comment_count'];
+            $post_status = $row['post_status'];
 
 
-                                echo "<tr>";
-                                echo    "<td>$post_id</td>";
-                                echo    "<td>$post_author</td>";
-                                echo    "<td>$post_title</td>";
-                                echo    "<td>$post_category_id</td>";
-                                echo    "<td>$post_status</td>";
-                                echo    "<td><img width='100' src='../images/$post_image' alt ='image'</td>";
-                                echo    "<td>$post_tags</td>";
-                                echo    "<td>$post_comment_count</td>";
-                                echo    "<td>$post_date</td>";
-                                echo    "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
-                                echo "</tr>";
-                            }
-                            
-                            if(isset($_GET['delete'])){
-                                $post_id = $_GET['post_id'];
-                                
-                                $query = "DELETE FROM posts WHERE post_id = {$post_id}";
-                                $delete_post_query = mysqli($connection, $query);
-                                
-                                $confirm_query($delete_post_query);
-                            }
+            echo "<tr>";
+            echo    "<td>$post_id</td>";
+            echo    "<td>$post_author</td>";
+            echo    "<td>$post_title</td>";
+            echo    "<td>$post_category_id</td>";
+            echo    "<td>$post_status</td>";
+            echo    "<td><img width='100' src='../images/$post_image' alt ='image'</td>";
+            echo    "<td>$post_tags</td>";
+            echo    "<td>$post_comment_count</td>";
+            echo    "<td>$post_date</td>";
+            echo    "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+            echo "</tr>";
+        }
 
-                            ?>
-                        </thead>
-                    </table>
+        if(isset($_GET['delete'])){
+            $post_id = $_GET['delete'];
+
+            $query = "DELETE FROM posts WHERE post_id = {$post_id}";
+            $delete_post_query = mysqli_query($connection, $query);
+
+            header('Location: '.$_SERVER['PHP_SELF']);
+            exit;
+
+            confirm_query($delete_post_query);
+        }
+
+        ?>
+    </tbody>
+</table>
