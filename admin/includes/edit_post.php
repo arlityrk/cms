c95723b46d6fd0b20365a7a278991e3fca8f5286
arlityrk <arlityrk@gmail.com>
2019-01-06 21:34:21 +0200

    Video 116.

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -22,6 +22,47 @@ if(isset($_GET["p_id"])){
         $post_status = $row['post_status'];
         $post_content = $row['post_content'];
     }
+
+    if(isset($_POST['update_post'])){
+        $post_title = $_POST["title"];
+        $post_category_id = $_POST["post_category"];
+        $author = $_POST["author"];
+        $post_status = $_POST["post_status"];
+        $post_image = $_FILES["image"]["name"];
+        $post_image_temp = $_FILES["image"]["tmp_name"];
+        $post_tags = $_POST["post_tags"];
+        $post_content = $_POST["post_content"];
+        $post_date = date("d-m-y");
+        
+        if(empty($post_image)){
+            
+            $query = "SELECT * FROM posts WHERE post_id = $get_post_id";
+            $select_image = mysqli_query($connection, $query);
+            
+             while($row = mysqli_fetch_assoc($select_image)){
+                 $post_image = $row['post_image'];
+             }
+            
+            
+        }
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        $query = "UPDATE posts SET ";
+        $query .= "post_title = '$post_title', ";
+        $query .= "post_category_id = '$post_category_id', ";
+        $query .= "post_author  = '$post_author', ";
+        $query .= "post_status  = '$post_status', ";
+        $query .= "post_image  = '$post_image', ";
+        $query .= "post_tags  = '$post_tags', ";
+        $query .= "post_date  = now(), ";
+        $query .= "post_content  = '$post_content' ";
+        $query .= "WHERE post_id = '$get_post_id'";
+        
+        $update_query = mysqli_query($connection, $query);
+
+        confirm_query($update_query);
+    }
 }
 
 
@@ -40,8 +81,8 @@ if(isset($_GET["p_id"])){
         <select name="post_category" id="post_category">
 
             <?php
-    
-               $query = "SELECT * FROM categories order by cat_id desc";
+
+    $query = "SELECT * FROM categories order by cat_id desc";
                $categories_query = mysqli_query($connection, $query);
 
                confirm_query($categories_query);
@@ -64,7 +105,7 @@ if(isset($_GET["p_id"])){
     </div>
 
     <div class="form-group">
-        <label for="post_status">Post Status</label>
+        <label for="post_status">Post Status</label>d
         <input value="<?php echo $post_status ?>" type="text" class="form-control" name="post_status">
     </div>
 
@@ -72,6 +113,7 @@ if(isset($_GET["p_id"])){
         <label for="post_image">Post Image</label>
         <br>
         <img width= "100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -85,7 +127,7 @@ if(isset($_GET["p_id"])){
     </div>
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+        <input type="submit" class="btn btn-primary" name="update_post" value="Update post">
     </div>
 
 </form>
