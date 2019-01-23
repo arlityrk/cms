5d52eb298fda091bc176e3d2519df2dc186aafb4
arlityrk <arlityrk@gmail.com>
2019-01-23 21:30:13 +0200

    SQL

--- DB/cms.sql
+++ DB/cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2019 at 08:00 PM
+-- Generation Time: Jan 23, 2019 at 08:28 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -64,7 +64,12 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
-(1, 1, 'Arli', 'asd@asd.ee', 'This is test', 'added', '2019-01-11');
+(7, 6, 'Mina', 'sdfs@asdafa.ee', 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.', 'approved', '2019-01-15'),
+(8, 6, 'Tere tali', 'bah@gas.ee', 'Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.', 'approved', '2019-01-15'),
+(9, 7, 'werfwerw', 'tali@tali.ee', 'sdgsdgs', 'approved', '2019-01-15'),
+(10, 6, 'Arli TÃ¼rk', 'arli@arli.ee', 'kommentaari sisu', 'approved', '2019-01-15'),
+(11, 6, 'keegi 2', 'asdga@sdfgsd.ee', 'sfdgasd', 'approved', '2019-01-17'),
+(12, 9, 'inimene', 'sdafhdsafh@sdfgsdfg.ee', 'sdfgdfg', 'unapproved', '2019-01-17');
 
 -- --------------------------------------------------------
 
@@ -90,9 +95,34 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES
-(6, 20, 'title', 'Arli', '2019-01-08', '1_4_60954763.jpg', 'Content', 'java', 4, 'Draft'),
-(7, 22, 'Pealkiri12322', 'Mina Ise', '2019-01-06', 'raamat.jpg', 'Suur sisu22244', 'tag66, tag232', 4, 'Public2'),
-(8, 19, 'New Post', 'Arli TÃ¼rk', '2019-01-08', '1_4_60954763.jpg', 'Big text', 'ARLI', 4, 'Published');
+(6, 20, 'title', 'Arli', '2019-01-08', '1_4_60954763.jpg', 'Content', 'java', 5, 'Draft'),
+(8, 21, 'New Post', 'Arli TÃ¼rk', '2019-01-18', '1_4_60954763.jpg', 'Big text', 'ARLI', 4, 'draft'),
+(9, 21, 'Test post', 'HÃ¤rra TÃ¼rk', '2019-01-18', 'kelk.jpg', 'lalalalaalala lalala allas as', 'java', 1, 'draft');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(3) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_firstname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_image` text NOT NULL,
+  `user_role` varchar(255) NOT NULL,
+  `randSalt` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_image`, `user_role`, `randSalt`) VALUES
+(1, 'arlityrk', 'test', 'arli', 'türk', 'arli@arli.ee', '', 'admin', '');
 
 --
 -- Indexes for dumped tables
@@ -117,6 +147,12 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -130,13 +166,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
